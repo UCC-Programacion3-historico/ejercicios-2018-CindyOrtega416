cf91b715d0bc4dfdc169fd80e58378ab7ac8b746
CindyOrtega416 <cindyortega416@gmail.com>
2018-08-15 11:38:59 +0000

    ejercicio 1 hecho en clase

--- U01_Recursividad/Ej-01/main.cpp
+++ U01_Recursividad/Ej-01/main.cpp
@@ -3,5 +3,12 @@
 
 int main() {
     std::cout << "Ejercicio 01/01\n" << std::endl;
+
+    std::cout << potencia(2,2) << std::endl;
+    std::cout << potencia(2,16) << std::endl;
+    std::cout << potencia(33,2) << std::endl;
+    std::cout << potencia(5,10) << std::endl;
+    std::cout << potencia(2,0) << std::endl;
+
     return 0;
 }
