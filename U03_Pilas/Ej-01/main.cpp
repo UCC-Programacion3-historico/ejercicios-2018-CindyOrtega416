1dd9fc69c47fbb45a6aaf5c701d97fe84b24edbd
CindyOrtega416 <cindyortega416@gmail.com>
2018-09-05 11:50:45 +0000

    Ejercicios 1 2 y 3

--- U03_Pilas/Ej-01/main.cpp
+++ U03_Pilas/Ej-01/main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "../Pila/Pila.h"
+#include <string.h>
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 03/01\n" << std::endl;
-    std::cin>>"Hola";
-    
-
+    char *palabra = const_cast<char *>("Prueba");
+    Pila<char> hola;
+    for(int i = 0; i<strlen(palabra) ; i++){
+        hola.push(palabra[i]);
+    }
+    for(int i = 0; i<strlen(palabra) ; i++){
+        cout << hola.pop();
+    }
     return 0;
 }
